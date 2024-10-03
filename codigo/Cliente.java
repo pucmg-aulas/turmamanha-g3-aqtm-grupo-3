6a5be0ab3fbcad49a226ac849717cfe6618c2c3c
João Gabriel Maia <maiajoaogabriel.contact@gmail.com>
2024-10-03 07:16:55 -0300

    Update Cliente.java

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Cliente {
     private String nome;
     private String endereco;
@@ -5,7 +7,8 @@ public class Cliente {
     private int cpf;
     private String email;
     private String identificador;
-    private Veiculo[] veiculos; 
+    private Veiculo[] veiculos;
+    private RegistroVeiculo[] registros;
     private int quantidadeVeiculos;
 
     public Cliente(String nome, String endereco, String telefone, int cpf, String email, String identificador, int capacidadeVeiculos) {
@@ -15,44 +18,31 @@ public class Cliente {
         this.cpf = cpf;
         this.email = email;
         this.identificador = identificador;
-        this.veiculos = new Veiculo[capacidadeVeiculos]; 
-        this.quantidadeVeiculos = 0; 
+        this.veiculos = new Veiculo[capacidadeVeiculos];
+        this.registros = new RegistroVeiculo[capacidadeVeiculos];
+        this.quantidadeVeiculos = 0;
     }
 
     public void incluirPessoa() {
-       
     }
 
     public void editarPessoa() {
-        
     }
 
     public void excluirPessoa() {
-        
     }
 
-    public void adicionarVeiculo(Veiculo veiculo) {
-        if (quantidadeVeiculos < veiculos.length) {
-            veiculos[quantidadeVeiculos] = veiculo;
-            quantidadeVeiculos++;
-        } else {
-            System.out.println("Não é possível adicionar mais veículos, capacidade máxima atingida.");
-        }
+    public void adicionarVeiculo(Veiculo veiculo, Date dataEntrada) {
     }
 
     public void excluirVeiculo(int indice) {
-        if (indice >= 0 && indice < quantidadeVeiculos) {
-            for (int i = indice; i < quantidadeVeiculos - 1; i++) {
-                veiculos[i] = veiculos[i + 1];
-            }
-            veiculos[quantidadeVeiculos - 1] = null;
-            quantidadeVeiculos--;
-        } else {
-            System.out.println("Índice inválido.");
-        }
     }
 
     public Veiculo[] getVeiculos() {
         return veiculos;
+    }
+
+    public RegistroVeiculo[] getRegistros() {
+        return registros;
     }
 }
