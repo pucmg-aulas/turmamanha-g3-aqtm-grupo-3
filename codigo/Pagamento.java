15c6f1c2cf8489598419436caa53870ab5ed7604
João Gabriel Maia <maiajoaogabriel.contact@gmail.com>
2024-10-03 07:21:17 -0300

    Update Pagamento.java

--- codigo/Pagamento.java
+++ codigo/Pagamento.java
@@ -5,16 +5,20 @@ public class Pagamento {
     private Date diaPagamento;
     private String metodoPagamento;
     
-    public static final double LIMITE_PRECO = 50.0;
+    public static final double LimitePreco = 50.0;
 
     public Pagamento(double valor, Date diaPagamento, String metodoPagamento) {
         this.valor = valor;
         this.diaPagamento = diaPagamento;
         this.metodoPagamento = metodoPagamento;
+    }
 
+    public void gerarPagamento(Registro registro, Date diaPagamento, String metodoPagamento) {
+        this.valor = registro.calcularValor();
+        this.diaPagamento = diaPagamento;
+        this.metodoPagamento = metodoPagamento;
     }
 
     public void gerarNotaFiscal() {
-
     }
 }
